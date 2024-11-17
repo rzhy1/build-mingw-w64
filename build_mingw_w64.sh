d780ca314e72e7c6d064d531ebe9711ff3be137c
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-11-18 01:39:05 +0800

    Update build_mingw_w64.sh

--- build_mingw_w64.sh
+++ build_mingw_w64.sh
@@ -22,6 +22,7 @@ export PATH="/usr/lib/ccache:$PATH"
 export CCACHE_DIR="$HOME/.ccache"
 ccache --set-config=cache_dir=$CCACHE_DIR
 echo "1111"
+which gcc
 sudo rm /usr/lib/ccache/gcc
 sudo ln -s /usr/bin/ccache /usr/lib/ccache/gcc
 which gcc
